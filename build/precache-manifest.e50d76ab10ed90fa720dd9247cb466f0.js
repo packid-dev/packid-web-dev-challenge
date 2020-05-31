288efdcc2ad787caeb71ae5a8e187772638a25a0
hes-ron <39659599+hes-ron@users.noreply.github.com>
2020-05-31 19:10:22 -0300

    Build

---
+++ build/precache-manifest.e50d76ab10ed90fa720dd9247cb466f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f52cebf60c493eacace7687d60f7692",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e049e521f925fa7518b0",
+    "url": "/static/css/main.b0269232.chunk.css"
+  },
+  {
+    "revision": "ce05311c3bf29ba2c76c",
+    "url": "/static/js/2.ddcb551c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ddcb551c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e049e521f925fa7518b0",
+    "url": "/static/js/main.601be6dc.chunk.js"
+  },
+  {
+    "revision": "46762919aebd44b5f4d5",
+    "url": "/static/js/runtime-main.3f262abf.js"
+  },
+  {
+    "revision": "509daa0d6cdcbc912efcdcbbd3f5c694",
+    "url": "/static/media/main-image.509daa0d.jpg"
+  }
+]);
